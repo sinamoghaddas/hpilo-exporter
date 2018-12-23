d2abf54fde76850a5e8e0a7c378b2c7272ee1ebe
simo <sina.moghaddas@gmail.com>
2018-12-23 14:35:43 +0330

    update exporter.py

--- src/hpilo_exporter/exporter.py
+++ src/hpilo_exporter/exporter.py
@@ -83,6 +83,9 @@ def iloGetMetrics(host, port, user, password):
 					prometheus_metrics.gauges[gauge].labels(product_name=product_name,
 										server_name=server_name).set(1)
 				else:
+                                        print(status[0])
+                                        print(status[1])
+                                        print("")
 					prometheus_metrics.gauges[gauge].labels(product_name=product_name,
 										server_name=server_name).set(2)
 
